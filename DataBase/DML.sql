bbd2ee75ea593577eae73e877aee20322d953d56
Pablo Osuna <42562674+PabloOsuna1997@users.noreply.github.com>
2023-02-05 00:48:30 -0600

    DOC: documentacion

--- DataBase/DML.sql
+++ DataBase/DML.sql
@@ -1,13 +1,20 @@
-INSERT INTO Account_Management.dbo.tblCurrency ( currency, description, user_registry, user_modified ) VALUES ('Q', 'Quezales', 'jposuna','jposuna'), ('D', 'Dolar', 'jposuna','jposuna');
 
-INSERT INTO Account_Management.dbo.tblPerson_Type ( code, description , user_registry, user_modified ) VALUES ('INDIVIDUAL', 'Persona Individual', 'jposuna','jposuna'), ('EMPRESA', 'Persona Tipo empresa', 'jposuna','jposuna');
+INSERT INTO Account_Management.dbo.tblCurrency ( currency, description, user_registry, user_modified ) VALUES ('Q', 'Quezales', 'sysdba','sysdba'), ('D', 'Dolar', 'sysdba','sysdba');
+
+INSERT INTO Account_Management.dbo.tblPerson_Type ( code, description , user_registry, user_modified ) VALUES ('INDIVIDUAL', 'Persona Individual', 'sysdba','sysdba'), ('EMPRESA', 'Persona Tipo empresa', 'sysdba','sysdba');
 
 INSERT INTO Account_Management.dbo.tblGender  ( gender, description , user_registry, user_modified ) 
 VALUES 
-('F', 'Femenino', 'jposuna','jposuna'),
-('M', 'Masculino', 'jposuna','jposuna');
+('F', 'Femenino', 'sysdba','sysdba'),
+('M', 'Masculino', 'sysdba','sysdba');
+
+INSERT INTO Account_Management.dbo.tblAccount_Type (name, description, user_registry, user_modified)
+VALUES
+('MONETARIA', 'CUENTA MONETARIA', 'sysdba', 'sysdba'),
+('AHORRO', 'CUENTA AHORRO', 'sysdba', 'sysdba');
 
 
 INSERT INTO Account_Management.dbo.tblBussiness_status  (  name , discriminator , description  , user_registry, user_modified ) 
 VALUES 
-('ACT', 'User','Usuario activo en el sistema', 'jposuna', 'jposuna');
+('ACT', 'User','Usuario activo en el sistema', 'sysdba', 'sysdba'),
+('DESC', 'User','Usuario inactivo en el sistema', 'sysdba', 'sysdba');
